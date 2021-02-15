113e3e363909965ef656eb47e3db898942a69821
Qyupang <hyunkyu0509@naver.com>
2021-02-15 15:05:14 +0900

    02.15 hyunkyu TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.Scanner;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int location1 = 0;
+        int location2 = -1;
+        
+        for (int i = 0; i < nums.length; i++){
+            int leftover = target - nums[i];
+            location1 = i;
+            for (int j = i+1; j < nums.length; j++){
+                if (nums[j] == leftover){
+                    location2 = j;
+                    break;
+                }
+            }
+            if (location2 != -1)
+                break;
+        }
+        
+        int[] result = {location1, location2};
+        
+        return result;
+    }
+}
+
