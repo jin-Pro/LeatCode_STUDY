3fc02d96c459c0ea0017446005db91d14899dfbd
Qyupang <hyunkyu0509@naver.com>
2021-02-21 00:01:43 +0900

    2021-02-19 LeetCode Easy No_206 USING_JAVA

---
+++ No_206_ReverseLinkedList.java
@@ -0,0 +1,20 @@
+package easy;
+
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ 
+public class No_206_ReverseLinkedList {
+        
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
+}
